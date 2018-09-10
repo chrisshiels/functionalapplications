4bfcbf473de26db1ed6b3002f6d3108b4989b672
Chris Shiels <chris@systems-engineers.co.uk>
2018-09-10 23:09:42 +0100

    Tidied test_main_singleargument(), test_main_multiplearguments() and test_main_stdin().

--- expr/test_expr.py
+++ expr/test_expr.py
@@ -155,28 +155,25 @@ def test_parse_success(stdout, stderr):
 
 
 def test_main_singleargument(stdin, stdout, stderr):
-  ret = expr.main(stdin, stdout, stderr, [ 'file.py',
-                                           '4 + 4 * 2' ])
-  assert ret == 0
+  assert expr.main(stdin, stdout, stderr, [ 'file.py',
+                                            '4 + 4 * 2' ]) == 0
   assert stdout.getvalue().rstrip() == '12'
   assert stderr.getvalue() == ''
 
 
 def test_main_multiplearguments(stdin, stdout, stderr):
-  ret = expr.main(stdin, stdout, stderr, [ 'file.py',
-                                           '4',
-                                           '+',
-                                           '4',
-                                           '*',
-                                           '2' ])
-  assert ret == 0
+  assert expr.main(stdin, stdout, stderr, [ 'file.py',
+                                            '4',
+                                            '+',
+                                            '4',
+                                            '*',
+                                            '2' ]) == 0
   assert stdout.getvalue().rstrip() == '12'
   assert stderr.getvalue() == ''
 
 
 def test_main_stdin(stdout, stderr):
   stdin = StringIO.StringIO('(6 + 6) / (2 + 2)')
-  ret = expr.main(stdin, stdout, stderr, [ 'file.py' ])
-  assert ret == 0
+  assert expr.main(stdin, stdout, stderr, [ 'file.py' ]) == 0
   assert stdout.getvalue().rstrip() == '3'
   assert stderr.getvalue() == ''
